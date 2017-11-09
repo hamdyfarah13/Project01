5ade2922a2b8ac419371339f806864659f755a8d
hamdyfarah13 <hamdyfarah13@gmail.com>
2017-11-09 12:33:32 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,76 @@
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-
-    if (this.hash !== ""){
+$(function() {
+  
+  //add products to shopping cart
+    $('.add-to-cart').click('button', function(event) {
       event.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
+  
+      var cartCount = parseInt($('.count').html(), 10);
+      $('.count').html(cartCount +=1);
+    });
+   
+  //flickity carousel
+    $('.main-carousel').flickity({
+      cellAlign: 'left',
+      contain: 'true',
+      accessibility: true,
+      resize: true,
+      freeScroll: true,
+      wrapAround: true,
+      prevNextButtons: false,
+    });
+  
+  //Smooth Scroll
+    $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // 
+              $target.focus(); 
+            };
+          });
+        }
+      }
+    });
+  
+  //email validator
+  $("#button").on('click', function(event) {
+    event.preventDefault();
+    console.log("working?")
+    var email = $('input').val();
+        var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        event.preventDefault();
+        console.log('User submitted the email address ' + email);
+  
+        if (filter.test(email)) {
+      alert('Thank you for subscribing, ' + email + '!');
+    
+        } else {
+            alert('Please enter a valid E-Mail address.');
+        }
+    });
+  
+  
+      //skip link bug fix
+    $(function() {
+      $("a[href^='#']").not("a[href='#']").click(function() {
+        $("#"+$(this).attr("href").slice(1)+"").focus();
       });
-    } 
+    });
   });
-});
-
-function validateForm() {
-  var eMail = document.forms["newsletter"]["email"].value;
-  var at = eMail.indexOf("@");
-  var dot = eMail.lastIndexOf(".");
-  if (at<1 || dot<at+2 || dot+2>=eMail.length) {
-      alert("Not a valid e-mail address");
-      return false;
-  }
-  else alert('Thank you for subscribing!');
-}
